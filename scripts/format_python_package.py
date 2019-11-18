e0f92ec66185699fcf777b90e6c6cb054647cf91
Andrew Hearin <ahearin@anl.gov>
2019-11-18 12:11:00 -0600

    Now modifying README as well

--- scripts/format_python_package.py
+++ scripts/format_python_package.py
@@ -10,12 +10,21 @@ pkgname = args.pkgname
 root_dirname_pkg = os.path.abspath(pkgname)
 
 
-#  Modify setup.py
-setup_fname = os.path.join(root_dirname_pkg, 'setup.py')
-all_raw_lines = []
-with open(setup_fname, 'r') as f:
-    new_raw_lines = [raw_line.replace('pkg_template', pkgname) for raw_line in f]
+def _replace_pkg_template_with_pkgname(pkg_fname, new_pkgname):
+    with open(pkg_fname, 'r') as f:
+        new_raw_lines = [raw_line.replace('pkg_template', new_pkgname) for raw_line in f]
 
-with open(setup_fname, 'w') as f:
-    for line in new_raw_lines:
-        f.write(line)
+    with open(pkg_fname, 'w') as f:
+        for line in new_raw_lines:
+            f.write(line)
+
+
+#  Search for 'pkg_template' and replace with pkgname in the following files:
+setup_fname = os.path.join(root_dirname_pkg, 'setup.py')
+readme_fname = os.path.join(root_dirname_pkg, 'README.md')
+fnames_to_modify = [setup_fname, readme_fname]
+for fname in fnames_to_modify:
+    _replace_pkg_template_with_pkgname(fname, pkgname)
+
+
+
