667cd84014de2629c0f621b1352b0711f6da38ef
Andrew Hearin <ahearin@anl.gov>
2020-02-10 12:25:45 -0600

    Updated shell script

--- scripts/create_pkg.sh
+++ scripts/create_pkg.sh
@@ -10,7 +10,7 @@ fi
 git clone https://github.com/aphearin/pkg_template.git
 
 cp pkg_template/scripts/format_python_package.py ./
-python format_python_package.py $1
+python format_python_package.py $2 $1
 
 rm format_python_package.py
 rm -rf pkg_template
